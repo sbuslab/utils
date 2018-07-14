87531bcd2fcb26c04a6764b0e80e2542f084dda7
kulikov <kulikov.dm@gmail.com>
2018-07-14 12:09:32 +0300

    + add Digest.hMacSHA256

--- src/main/java/com/sbuslab/utils/Digest.java
+++ src/main/java/com/sbuslab/utils/Digest.java
@@ -1,7 +1,13 @@
 package com.sbuslab.utils;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
+import org.apache.commons.codec.binary.Hex;
 
 
 public class Digest {
@@ -28,6 +34,13 @@ public class Digest {
 
     public static String sha256(byte[] message) {
         return digest("SHA-256", message);
+    }
+
+    public static String hMacSHA256(final String message, final String key) throws InvalidKeyException, NoSuchAlgorithmException {
+        final Mac hmac = Mac.getInstance("HmacSHA256");
+        hmac.init(new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "HmacSHA256"));
+
+        return new String(Hex.encodeHex(hmac.doFinal(message.getBytes(StandardCharsets.UTF_8))));
     }
 
     private static String digest(String digestName, byte[] message) {

22988a172a121fcdcf2ef05ef2679e1898990230
Andrey Pshenkin <a.pshenkin@rambler-co.ru>
2019-04-25 14:27:00 +0300

    Add encrypt decrypt aes

--- src/main/java/com/sbuslab/utils/Digest.java
+++ src/main/java/com/sbuslab/utils/Digest.java
@@ -1,11 +1,13 @@
 package com.sbuslab.utils;
 
-import javax.crypto.Mac;
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
+import java.util.Arrays;
+import java.util.UUID;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
@@ -60,6 +62,54 @@ public class Digest {
         return Base64.decodeBase64(data);
     }
 
+    public static String decryptAes(String secret, String data) {
+        byte[] cipherData = java.util.Base64.getDecoder().decode(data);
+        byte[] saltData = Arrays.copyOfRange(cipherData, 8, 16);
+
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            final byte[][] keyAndIV = generateKeyAndIV(32, 16, 1, saltData, secret.getBytes(StandardCharsets.UTF_8), md5);
+            SecretKeySpec key = new SecretKeySpec(keyAndIV[0], "AES");
+            IvParameterSpec iv = new IvParameterSpec(keyAndIV[1]);
+
+            byte[] encrypted = Arrays.copyOfRange(cipherData, 16, cipherData.length);
+            Cipher aesCBC = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            aesCBC.init(Cipher.DECRYPT_MODE, key, iv);
+            byte[] decryptedData = aesCBC.doFinal(encrypted);
+
+            return new String(decryptedData, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String encryptAes(String secret, String text) {
+        try {
+            final String Transformation = "AES/CBC/PKCS5Padding";
+            final byte[] SaltedPrefix = "Salted__".getBytes(StandardCharsets.ISO_8859_1);
+            final int SaltSize = 8;
+
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            final byte[] salt = Arrays.copyOf(UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8), SaltSize);
+            byte[][] keyAndIv = generateKeyAndIV(32, 16, 1, salt, secret.getBytes(StandardCharsets.UTF_8), md5);
+            SecretKeySpec key = new SecretKeySpec(keyAndIv[0], "AES");
+            IvParameterSpec iv = new IvParameterSpec(keyAndIv[1]);
+
+            Cipher cipher = Cipher.getInstance(Transformation);
+            cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+
+            byte[] cipherData = cipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
+
+            byte[] saltedCipherData = new byte[SaltedPrefix.length + SaltSize + cipherData.length];
+            ByteBuffer buf = ByteBuffer.wrap(saltedCipherData);
+            buf.put(SaltedPrefix).put(salt).put(cipherData);
+
+            return java.util.Base64.getEncoder().encodeToString(saltedCipherData);
+        } catch (Exception e) {
+           throw new RuntimeException(e);
+        }
+    }
+
     private static String digest(String digestName, byte[] message) {
         try {
             return toHexString(MessageDigest.getInstance(digestName).digest(message));
@@ -81,4 +131,53 @@ public class Digest {
 
         return hexString.toString();
     }
+
+    private static byte[][] generateKeyAndIV(int keyLength, int ivLength, int iterations, byte[] salt, byte[] password, MessageDigest md) {
+
+        int digestLength = md.getDigestLength();
+        int requiredLength = (keyLength + ivLength + digestLength - 1) / digestLength * digestLength;
+        byte[] generatedData = new byte[requiredLength];
+        int generatedLength = 0;
+
+        try {
+            md.reset();
+
+            // Repeat process until sufficient data has been generated
+            while (generatedLength < keyLength + ivLength) {
+
+                // Digest data (last digest if available, password data, salt if available)
+                if (generatedLength > 0)
+                    md.update(generatedData, generatedLength - digestLength, digestLength);
+                md.update(password);
+                if (salt != null)
+                    md.update(salt, 0, 8);
+                md.digest(generatedData, generatedLength, digestLength);
+
+                // additional rounds
+                for (int i = 1; i < iterations; i++) {
+                    md.update(generatedData, generatedLength, digestLength);
+                    md.digest(generatedData, generatedLength, digestLength);
+                }
+
+                generatedLength += digestLength;
+            }
+
+            // Copy key and IV into separate byte arrays
+            byte[][] result = new byte[2][];
+            result[0] = Arrays.copyOfRange(generatedData, 0, keyLength);
+            if (ivLength > 0)
+                result[1] = Arrays.copyOfRange(generatedData, keyLength, keyLength + ivLength);
+
+            return result;
+
+        } catch (DigestException e) {
+            throw new RuntimeException(e);
+
+        } finally {
+            // Clean out temporary data
+            Arrays.fill(generatedData, (byte) 0);
+        }
+    }
+
+
 }

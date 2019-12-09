b8c3ccb5ec8118eaddf0c67c26f0c4b4c241c559
kulikov <kulikov.dm@gmail.com>
2019-12-09 12:27:16 +0300

    JacksonBeanRowMapper: optimize json reader

--- src/main/java/com/sbuslab/utils/db/JacksonBeanRowMapper.java
+++ src/main/java/com/sbuslab/utils/db/JacksonBeanRowMapper.java
@@ -1,54 +1,55 @@
 package com.sbuslab.utils.db;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.ResultSetMetaData;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
 
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.postgresql.util.PGobject;
-import org.springframework.jdbc.core.ColumnMapRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.JdbcUtils;
 
 import com.sbuslab.utils.StringUtils;
 
 
 public class JacksonBeanRowMapper<T> implements RowMapper<T> {
 
-    private final Class<T> mappedClass;
+    private final JavaType mappedType;
     private final ObjectMapper objectMapper;
     private final BiFunction<Map<String, Object>, T, T> extraFieldsMapper;
-
-    private final ColumnMapRowMapper columnMapper = new ColumnMapRowMapper();
 
     public JacksonBeanRowMapper(Class<T> mappedClass, ObjectMapper objectMapper) {
         this(mappedClass, objectMapper, null);
     }
 
     public JacksonBeanRowMapper(Class<T> mappedClass, ObjectMapper objectMapper, BiFunction<Map<String, Object>, T, T> extraFieldsMapper) {
-        this.mappedClass = mappedClass;
+        this.mappedType = objectMapper.getTypeFactory().constructType(mappedClass);
         this.objectMapper = objectMapper;
         this.extraFieldsMapper = extraFieldsMapper;
     }
 
     @Override
-    public T mapRow(ResultSet rs, int rowNum) throws SQLException {
+    public T mapRow(ResultSet rs, int rowNum) {
         try {
-            Map<String, Object> row = columnMapper.mapRow(rs, rowNum);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+            Map<String, Object> map = new HashMap<>(columnCount);
 
-            Map<String, Object> map = new HashMap<>(row.size());
-            for (Map.Entry<String, Object> entry : row.entrySet()) {
-                Object value = entry.getValue() instanceof PGobject ?
-                    objectMapper.readTree(((PGobject) entry.getValue()).getValue())
-                    : entry.getValue();
-                map.put(StringUtils.toCamelCase(entry.getKey()), value);
+            for (int i = 1; i <= columnCount; i++) {
+                String column = JdbcUtils.lookupColumnName(rsmd, i);
+                Object value = JdbcUtils.getResultSetValue(rs, i);
+
+                map.put(StringUtils.toCamelCase(column),
+                    value instanceof PGobject ? objectMapper.readTree(((PGobject) value).getValue()) : value);
             }
 
-            T bean = objectMapper.convertValue(map, mappedClass);
+            T bean = objectMapper.convertValue(map, mappedType);
 
             if (extraFieldsMapper != null) {
-                return extraFieldsMapper.apply(row, bean);
+                return extraFieldsMapper.apply(map, bean);
             }
 
             return bean;

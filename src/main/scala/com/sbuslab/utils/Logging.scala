7a01b9d52ca3bd276cd6e85fbbfc37f9e66eee54
kulikov <kulikov.dm@gmail.com>
2020-02-04 22:12:49 +0300

    add slog: logger with implicit sbus Context: write correlation_id to logs

--- src/main/scala/com/sbuslab/utils/Logging.scala
+++ src/main/scala/com/sbuslab/utils/Logging.scala
@@ -1,12 +1,27 @@
 package com.sbuslab.utils
 
-import com.typesafe.scalalogging.Logger
-import org.slf4j.LoggerFactory
+import com.typesafe.scalalogging.{CanLog, Logger}
+import org.slf4j.{LoggerFactory, MDC}
+
+import com.sbuslab.sbus.Context
 
 
 trait Logging {
 
+  implicit case object CanLogSbusContext extends CanLog[Context] {
+    override def logMessage(originalMsg: String, context: Context): String = {
+      MDC.put("correlation_id", context.correlationId)
+      originalMsg
+    }
+
+    override def afterLog(context: Context): Unit = {
+      MDC.remove("correlation_id")
+    }
+  }
+
   protected val log = getLogger(this.getClass.getName)
+
+  protected val slog = Logger.takingImplicit[Context](LoggerFactory.getLogger(this.getClass.getName))
 
   def getLogger(name: String) = Logger(LoggerFactory.getLogger(name))
 }

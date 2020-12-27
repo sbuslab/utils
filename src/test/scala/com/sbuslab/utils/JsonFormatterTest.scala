2cac8289952f0a746b72ed8ce586c405e480ee8a
kulikov <kulikov.dm@gmail.com>
2020-12-27 17:48:25 +0300

    add json tests

--- src/test/scala/com/sbuslab/utils/JsonFormatterTest.scala
+++ src/test/scala/com/sbuslab/utils/JsonFormatterTest.scala
@@ -15,4 +15,17 @@ class JsonFormatterTest extends FunSuite {
     assert(num.toString == "1E+3")
     assert(JsonFormatter.serialize(num) == "\"1000\"")
   }
+
+  test("should serialize null BigDecimal") {
+
+    val o = TestMe("test1", null)
+
+    assert(JsonFormatter.serialize(o) == """{"name":"test1"}""")
+  }
 }
+
+
+case class TestMe(
+  name: String,
+  num: BigDecimal = null
+)

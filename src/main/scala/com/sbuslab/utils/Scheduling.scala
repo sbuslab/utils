a57986db15964603fa34da17e3578c881a980f72
Alex Parlett <alexparlett@gmail.com>
2022-07-18 10:49:45 +0100

    NOJIRA Lets maintain backwards compatibility as much as possible.

--- src/main/scala/com/sbuslab/utils/Scheduling.scala
+++ src/main/scala/com/sbuslab/utils/Scheduling.scala
@@ -3,20 +3,17 @@ package com.sbuslab.utils
 import com.sbuslab.model.scheduler.ScheduleCommand
 import com.sbuslab.sbus.Sbus
 
+import scala.concurrent.Future
+
 trait Scheduling {
 
 
   def sbus: Sbus
 
-  def schedule(routingKey: String, period: Long, body: Option[Any] = Option.empty, scheduleId: Option[String] = Option.empty, atTime: Option[Long] = Option.empty): Unit = {
-    val context = sbus.sign(routingKey, body)
+  def schedule(scheduleCommand: ScheduleCommand): Future[Unit] = {
+    val context = sbus.sign(scheduleCommand.getRoutingKey, scheduleCommand.getBody)
 
-    sbus.command("scheduler.schedule", ScheduleCommand.builder
-      .scheduleId(scheduleId.getOrElse(routingKey))
-      .atTime(atTime.getOrElse(System.currentTimeMillis))
-      .period(period)
-      .routingKey(routingKey)
-      .body(body.orNull)
+    sbus.command("scheduler.schedule", scheduleCommand.toBuilder
       .origin(context.origin)
       .signature(context.signature)
       .build)

8afc4ee5b015e15c85eb883277b3897a4799229e
Alex Parlett <alexparlett@gmail.com>
2022-07-18 10:40:00 +0100

    NOJIRA Support atTime

--- src/main/scala/com/sbuslab/utils/Scheduling.scala
+++ src/main/scala/com/sbuslab/utils/Scheduling.scala
@@ -8,12 +8,12 @@ trait Scheduling {
 
   def sbus: Sbus
 
-  def schedule(routingKey: String, period: Long, body: Option[Any] = Option.empty, scheduleId: Option[String] = Option.empty): Unit = {
+  def schedule(routingKey: String, period: Long, body: Option[Any] = Option.empty, scheduleId: Option[String] = Option.empty, atTime: Option[Long] = Option.empty): Unit = {
     val context = sbus.sign(routingKey, body)
 
     sbus.command("scheduler.schedule", ScheduleCommand.builder
       .scheduleId(scheduleId.getOrElse(routingKey))
-      .atTime(System.currentTimeMillis())
+      .atTime(atTime.getOrElse(System.currentTimeMillis))
       .period(period)
       .routingKey(routingKey)
       .body(body.orNull)

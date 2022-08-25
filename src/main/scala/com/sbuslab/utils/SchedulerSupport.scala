4cb59dc37db7a92f253ea2117a8c030b6e0a0c7e
Alex Parlett <alexparlett@gmail.com>
2022-08-25 16:57:18 +0100

    PRIM-32 Update what is signed

--- src/main/scala/com/sbuslab/utils/SchedulerSupport.scala
+++ src/main/scala/com/sbuslab/utils/SchedulerSupport.scala
@@ -2,26 +2,34 @@ package com.sbuslab.utils
 
 import scala.concurrent.Future
 
+import com.sbuslab.model.Message
 import com.sbuslab.model.scheduler.ScheduleCommand
 import com.sbuslab.sbus.Sbus
-
 
 trait SchedulerSupport {
 
   implicit class SbusSchedulerSupport(sbus: Sbus) {
 
-    def schedule(routingKey: String, period: java.lang.Long = null, body: Any = null, scheduleId: String = null, atTime: java.lang.Long = null): Future[Unit] = {
-      val context = sbus.sign(routingKey, body)
+    def schedule(
+      routingKey: String,
+      period: java.lang.Long = null,
+      body: Any              = null,
+      scheduleId: String     = null,
+      atTime: java.lang.Long = null): Future[Unit] = {
+      val context = sbus.sign(routingKey, new Message(routingKey, body))
 
-      sbus.command("scheduler.schedule", ScheduleCommand.builder
-        .scheduleId(scheduleId)
-        .atTime(atTime)
-        .period(period)
-        .routingKey(routingKey)
-        .body(body)
-        .origin(context.origin)
-        .signature(context.signature)
-        .build)
+      sbus.command(
+        "scheduler.schedule",
+        ScheduleCommand.builder
+          .scheduleId(scheduleId)
+          .atTime(atTime)
+          .period(period)
+          .routingKey(routingKey)
+          .body(body)
+          .origin(context.origin)
+          .signature(context.signature)
+          .build
+      )
     }
   }
 }

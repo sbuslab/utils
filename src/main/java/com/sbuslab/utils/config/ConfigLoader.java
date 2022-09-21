0d210fc083ba8a7298fa88fd796434ee70509f8c
Edgaras Makarovas <edgaras.makarovas@copper.co>
2022-09-21 14:52:53 +0200

    STTINT-724 Improve config error logging Modified `ConfigLoader` to log any errors that occur when parsing the configuration.

--- src/main/java/com/sbuslab/utils/config/ConfigLoader.java
+++ src/main/java/com/sbuslab/utils/config/ConfigLoader.java
@@ -24,53 +24,58 @@ public class ConfigLoader {
     public final static Config INSTANCE = load();
 
     private static Config load() {
-        String localConfigFiles = System.getProperty("config.localfile",
-            Optional.ofNullable(System.getenv("SBUS_CONFIG_LOCALFILE")).orElse(""))
-                .replace("/", File.separator)
-                .replace("\\", File.separator);
-
         try {
-            String secretsUrl = System.getenv("SECRET_CONFIG_URL");
+            String localConfigFiles = System.getProperty("config.localfile",
+                            Optional.ofNullable(System.getenv("SBUS_CONFIG_LOCALFILE")).orElse(""))
+                    .replace("/", File.separator)
+                    .replace("\\", File.separator);
 
-            if (secretsUrl != null && !secretsUrl.isEmpty()) {
-                Config secrets = ConfigFactory.parseURL(new URL(secretsUrl));
-
-                secrets.entrySet().forEach(entry ->
-                    System.setProperty("SECRET_" + entry.getKey().toUpperCase().replaceAll("[^A-Z0-9]+", "_"), entry.getValue().unwrapped().toString())
-                );
-            }
-        } catch (Exception e) {
-            log.warn("Error on load configs from url " + System.getenv("SECRET_CONFIG_URL") + ": " + e.getMessage(), e);
-        }
-
-        Config resultConfig = ConfigFactory.load();
-
-        for (String filePath : localConfigFiles.split(":")) {
             try {
-                File file = new File(FileUtils.getFileUrl(filePath).toURI());
+                String secretsUrl = System.getenv("SECRET_CONFIG_URL");
 
-                if (!file.exists()) {
-                    log.warn(file.getAbsolutePath() + " is not found, skip");
-                } else {
-                    resultConfig = ConfigFactory.parseFile(file).withFallback(resultConfig);
+                if (secretsUrl != null && !secretsUrl.isEmpty()) {
+                    Config secrets = ConfigFactory.parseURL(new URL(secretsUrl));
+
+                    secrets.entrySet().forEach(entry ->
+                            System.setProperty("SECRET_" + entry.getKey().toUpperCase().replaceAll("[^A-Z0-9]+", "_"), entry.getValue().unwrapped().toString())
+                    );
                 }
-            } catch (FileNotFoundException | URISyntaxException e) {
-                log.warn(filePath + " is not found, skip");
-            }
-        }
-
-        resultConfig = ConfigFactory.defaultOverrides().withFallback(resultConfig);
-
-        String extraConfigUrl = resultConfig.resolve().getString("sbuslab.config.external-url");
-
-        if (!extraConfigUrl.isEmpty()) {
-            try {
-                resultConfig = ConfigFactory.parseURL(new URL(extraConfigUrl)).withFallback(resultConfig);
             } catch (Exception e) {
-                log.warn("Error on load external config from url: " + extraConfigUrl + ". " + e.getMessage(), e);
+                log.warn("Error on load configs from url " + System.getenv("SECRET_CONFIG_URL") + ": " + e.getMessage(), e);
             }
-        }
 
-        return resultConfig.resolve();
+            Config resultConfig = ConfigFactory.load();
+
+            for (String filePath : localConfigFiles.split(":")) {
+                try {
+                    File file = new File(FileUtils.getFileUrl(filePath).toURI());
+
+                    if (!file.exists()) {
+                        log.warn(file.getAbsolutePath() + " is not found, skip");
+                    } else {
+                        resultConfig = ConfigFactory.parseFile(file).withFallback(resultConfig);
+                    }
+                } catch (FileNotFoundException | URISyntaxException e) {
+                    log.warn(filePath + " is not found, skip");
+                }
+            }
+
+            resultConfig = ConfigFactory.defaultOverrides().withFallback(resultConfig);
+
+            String extraConfigUrl = resultConfig.resolve().getString("sbuslab.config.external-url");
+
+            if (!extraConfigUrl.isEmpty()) {
+                try {
+                    resultConfig = ConfigFactory.parseURL(new URL(extraConfigUrl)).withFallback(resultConfig);
+                } catch (Exception e) {
+                    log.warn("Error on load external config from url: " + extraConfigUrl + ". " + e.getMessage(), e);
+                }
+            }
+
+            return resultConfig.resolve();
+        } catch (Exception e) {
+            log.error("An error occurred while processing the configuration!", e);
+            throw e;
+        }
     }
 }
